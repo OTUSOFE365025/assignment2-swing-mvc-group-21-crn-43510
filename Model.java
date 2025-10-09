ecdb4150bdf6aa0b413cf603c03514b45720bfbb
Aaraan Mahmood <151668629+aairhann17@users.noreply.github.com>
2025-10-08 23:42:15 -0400

    Create Model.java

---
+++ Model.java
@@ -0,0 +1,25 @@
+public class Model {
+	 private String fname;
+	 private String lname;
+	 
+	 public Model(String fname, String lname) {
+	  this.fname = fname;
+	  this.lname = lname;
+	 }
+	 
+	 public String getFname() {
+	  return fname;
+	 }
+	 
+	 public void setFname(String fname) {
+	  this.fname = fname;
+	 }
+	 
+	 public String getLname() {
+	  return lname;
+	 }
+	 
+	 public void setLname(String lname) {
+	  this.lname = lname;
+	 }
+}
